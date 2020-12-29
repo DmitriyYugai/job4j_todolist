524d79641a57b316445990adee22f72675f70dd9
Dmitriy Yugai <studentnstu97@mail.ru>
2020-12-29 12:22:13 +0700

    добавил сортировку списка полученных задач

--- src/main/java/servlets/ItemServlet.java
+++ src/main/java/servlets/ItemServlet.java
@@ -10,12 +10,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
 
 public class ItemServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Collection<Item> items = ItemDao.instOf().findAll();
+        List<Item> items = ItemDao.instOf().findAll();
+        items.sort(Comparator.comparing(Item::getId));
         for(Item item : items) {
             item.getUser().setPassword(null);
         }
